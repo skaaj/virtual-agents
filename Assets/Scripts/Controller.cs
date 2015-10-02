5435f17b3c0141338d400953605d7054661f96c4
Benjamin Denom <ben.denom@gmail.com>
2015-10-02 09:36:49 +0200

    Add a grass plane

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+
+        rb.AddForce(dx, 0, dz);
+	}
+}
