69a2160ccadef78765e3f70a1b1ac3f8f9342498
Benjamin Denom <ben.denom@gmail.com>
2015-10-30 10:44:44 +0100

    Agents can move and avoid obstacles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dt = Time.deltaTime;
+		float dx = Input.GetAxis ("Horizontal") * velocity * dt;
+		float dz = Input.GetAxis ("Vertical") * velocity * dt;
+
+		transform.Translate(new Vector3 (dx, 0.0f, dz));
+	}
+}
